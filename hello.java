941d94efdf19595505eb5af3a8d5726dea75d8e6
Nick roy <nroy08232@gmail.com>
2024-10-02 12:23:16 +0600

    add new line

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 public class Hello {
   public static void main(string[] args){
    system.out.printIn("VS Code is Awsome...")
+   display('Nick roy')
+  }
+  public static void display(string name){
+   system.out.printIn("Welcome " + name)
   }
 }
